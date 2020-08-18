8de05a50fe4f3a10bdec4040534c0aab711333ab
Lucas Alvarenga <lucasalvarenga020@gmail.com>
2020-08-18 09:27:13 -0300

    style: organizing the code in ReviewsResult.kt

--- app/src/main/java/com/alvarengadev/moviereviews/data/api/network/reviews/response/ReviewsResult.kt
+++ app/src/main/java/com/alvarengadev/moviereviews/data/api/network/reviews/response/ReviewsResult.kt
@@ -2,4 +2,6 @@ package com.alvarengadev.moviereviews.data.api.network.reviews.response
 
 import com.squareup.moshi.Json
 
-data class ReviewsResult (@field:Json(name = "results") val results: List<ReviewBodyResponse>)
+data class ReviewsResult (
+    @field:Json(name = "results") val results: List<ReviewBodyResponse>
+)
