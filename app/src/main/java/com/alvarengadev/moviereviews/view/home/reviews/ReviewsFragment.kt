0f1e0ae8834b7cf6696b0fa412c709a7b40bcd8d
Lucas Alvarenga <lucasalvarenga020@gmail.com>
2020-08-18 09:21:41 -0300

    style: organizing the code in ReviewsFragment.kt

--- app/src/main/java/com/alvarengadev/moviereviews/view/home/reviews/ReviewsFragment.kt
+++ app/src/main/java/com/alvarengadev/moviereviews/view/home/reviews/ReviewsFragment.kt
@@ -34,7 +34,11 @@ class ReviewsFragment : Fragment(),
         }
     )
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         return inflater.inflate(R.layout.fragment_reviews, container, false)
     }
 
